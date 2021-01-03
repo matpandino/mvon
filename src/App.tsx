c82e7d57b41f556c108c402ccaffa75dda0d292a
Mateus Pandino <matpandino@gmail.com>
2021-01-03 20:22:35 -0300

    Add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from 'styled-components';
 import {useColorScheme} from 'react-native';
 
 import themes from './styles/themes';
+import Home from './screens/Home';
+import Exercises from './screens/Exercises';
+import Profile from './screens/Profile';
 
-import Home from './components/Home';
-
-declare const global: {HermesInternal: null | {}};
+const {Navigator, Screen} = createBottomTabNavigator();
 
 const App = () => {
   const deviceTheme = useColorScheme();
@@ -14,9 +20,51 @@ const App = () => {
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Home />
-      </ThemeProvider>
+      <NavigationContainer>
+        <ThemeProvider theme={theme}>
+          <Navigator
+            screenOptions={({route}) => ({
+              tabBarIcon: ({focused, size}) => {
+                let iconName = 'heart';
+
+                switch (route.name) {
+                  case 'Home':
+                    iconName = focused ? 'heart' : 'heart-outline';
+                    break;
+                  case 'Exercises':
+                    iconName = focused ? 'ios-barbell' : 'ios-barbell-outline';
+                    break;
+                  case 'Profile':
+                    iconName = focused
+                      ? 'person-circle-sharp'
+                      : 'person-circle-outline';
+                    break;
+
+                  default:
+                    break;
+                }
+
+                return (
+                  <Icon name={iconName} size={size} color={theme.colors.text} />
+                );
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: 'tomato',
+              inactiveTintColor: 'gray',
+              style: {
+                backgroundColor: theme.colors.background,
+                borderTopWidth: 0.2,
+                borderTopColor: '#414141',
+              },
+              showLabel: false,
+            }}>
+            <Screen name="Home" component={Home} />
+            <Screen name="Exercises" component={Exercises} />
+            <Screen name="Profile" component={Profile} />
+          </Navigator>
+        </ThemeProvider>
+      </NavigationContainer>
     </>
   );
 };
